4eb81a020182e0e062672012fafb8af068b6d235
Tim Yuen <timothy.yuen22@gmail.com>
2020-07-20 21:13:16 -0400

    Update to use v0.9 of bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,18 @@
-#[macro_use]
-extern crate gdnative as godot;
+use gdnative::prelude::*;
 
 extern crate rand;
 
 mod ball;
-mod hello_world;
 mod paddle;
 mod utils;
 
-use self::{ball::Ball, hello_world::HelloWorld, paddle::Paddle};
+use self::{ball::Ball, paddle::Paddle};
 
 // Function that registers all exposed classes to Godot
-fn init(handle: godot::init::InitHandle) {
-    handle.add_class::<HelloWorld>();
+fn init(handle: InitHandle) {
     handle.add_class::<Paddle>();
     handle.add_class::<Ball>();
 }
 
 // macros that create the entry-points of the dynamic library.
-godot_gdnative_init!();
-godot_nativescript_init!(init);
-godot_gdnative_terminate!();
+godot_init!(init);
--- src/paddle.rs
+++ src/paddle.rs
@@ -1,26 +1,25 @@
-use godot::{
-    init::{Property, PropertyHint, PropertyUsage},
-    user_data::MutexData,
-    GodotString, Input, KinematicBody2D, NodePath, Sprite, Vector2,
+use gdnative::{
+    nativescript::init::property::{EnumHint, FloatHint, RangeHint, StringHint, Usage},
+    object::Ref,
+    prelude::{
+        godot_print, methods, user_data::MutexData, GodotString, Input, KinematicBody2D, NodePath,
+        Sprite, Vector2,
+    },
 };
 
-use crate::utils::{clamp, lerp};
-
-const BASE_SPEED: f32 = 100.0;
-const MAX_VELOCITY: f32 = 500.0;
-const MIN_VELOCITY: f32 = -500.0;
+const BASE_SPEED: f32 = 200.0;
 
 pub struct Paddle {
     side: String,
     speed: f32,
     target_movement: f32,
     linear_velocity: Vector2,
-    sprite: Sprite,
+    sprite: Option<Ref<Sprite, gdnative::thread_access::Unique>>,
 }
 
 unsafe impl Send for Paddle {}
 
-impl godot::NativeClass for Paddle {
+impl gdnative::prelude::NativeClass for Paddle {
     type Base = KinematicBody2D;
     type UserData = MutexData<Paddle>;
 
@@ -28,82 +27,77 @@ impl godot::NativeClass for Paddle {
         "Paddle"
     }
 
-    fn init(_owner: Self::Base) -> Self {
-        Self::_init()
+    fn init(owner: &Self::Base) -> Self {
+        Self::new(owner)
     }
 
-    fn register_properties(_builder: &godot::init::ClassBuilder<Self>) {
-        _builder.add_property(Property {
-            name: "Side",
-            default: GodotString::from_str("none"),
-            hint: PropertyHint::Enum {
-                values: &["right", "left", "none"],
-            },
-            getter: |this: &Paddle| GodotString::from_str(this.side.clone()),
-            setter: |this: &mut Paddle, v: GodotString| this.side = v.to_string(),
-            usage: PropertyUsage::DEFAULT,
-        });
+    fn register_properties(builder: &gdnative::nativescript::init::ClassBuilder<Self>) {
+        builder
+            .add_property("Side")
+            .with_default(GodotString::from_str("none"))
+            .with_hint(StringHint::Enum(EnumHint::new(vec![
+                "right".into(),
+                "left".into(),
+                "none".into(),
+            ])))
+            .with_getter(|this: &Paddle, _owner: &KinematicBody2D| {
+                GodotString::from_str(this.side.clone())
+            })
+            .with_setter(|this: &mut Paddle, _owner: &KinematicBody2D, v| this.side = v.to_string())
+            .with_usage(Usage::DEFAULT)
+            .done();
 
-        _builder.add_property(Property {
-            name: "Speed",
-            default: BASE_SPEED,
-            hint: PropertyHint::Range {
-                range: 0.0..500.0,
-                step: 1.0,
-                slider: true,
-            },
-            getter: |this: &Paddle| this.speed,
-            setter: |this: &mut Paddle, v| this.speed = v,
-            usage: PropertyUsage::DEFAULT,
-        });
+        builder
+            .add_property("Speed")
+            .with_default(BASE_SPEED)
+            .with_hint(FloatHint::from(RangeHint::new(0.0, 500.0).with_step(1.0)))
+            .with_getter(|this: &Paddle, _owner: &KinematicBody2D| this.speed)
+            .with_setter(|this: &mut Paddle, _owner: &KinematicBody2D, v| this.speed = v)
+            .with_usage(Usage::DEFAULT)
+            .done();
     }
 }
 
 #[methods]
 impl Paddle {
-    fn _init() -> Self {
+    fn new(_owner: &KinematicBody2D) -> Self {
         Paddle {
             side: "none".to_string(),
             speed: BASE_SPEED,
             target_movement: 0.0,
             linear_velocity: Vector2::new(0.0, 0.0),
-            sprite: Sprite::new(),
+            sprite: None,
         }
     }
 
     #[export]
-    unsafe fn _ready(&mut self, mut owner: KinematicBody2D) {
-        owner.set_physics_process(true);
-        // TODO scary cast to KinematicBody2D that probably works
-        // self.kb2d = mem::transmute::<Node, godot::KinematicBody2D>(
-        //     owner
-        //         .get_node(NodePath::from_str("KinematicBody2D"))
-        //         .expect("Missing KinematicBody2D"),
-        // );
-
-        self.sprite = owner
-            .get_node(NodePath::from_str("Sprite"))
-            .expect("Missing Sprite node")
-            .cast::<Sprite>()
-            .expect("Unable to cast to Sprite");
+    unsafe fn _ready(&mut self, owner: &KinematicBody2D) {
+        self.sprite = Some(
+            owner
+                .get_node(NodePath::from_str("Sprite"))
+                .unwrap()
+                .assume_unique()
+                .cast::<Sprite>()
+                .unwrap(),
+        );
 
         godot_print!("Paddle created!");
     }
 
     #[export]
-    unsafe fn _physics_process(&mut self, mut owner: KinematicBody2D, delta: f64) {
-        let initial_position: Vector2 = owner.get_global_position();
+    unsafe fn _physics_process(&mut self, owner: &KinematicBody2D, _delta: f64) {
+        let initial_position: Vector2 = owner.global_position();
         self.target_movement *= self.speed;
         self.linear_velocity.y = self.target_movement;
         self.linear_velocity =
             owner.move_and_slide(self.linear_velocity, Vector2::zero(), false, 4, 0.7, true);
-        let new_position: Vector2 = owner.get_global_position();
+        let new_position: Vector2 = owner.global_position();
         // Godot can't lock objects to a specific axis, so we do that manually
         owner.set_global_position(Vector2::new(initial_position.x, new_position.y));
     }
 
     #[export]
-    unsafe fn _process(&mut self, mut owner: KinematicBody2D, delta: f64) {
+    unsafe fn _process(&mut self, _owner: &KinematicBody2D, _delta: f64) {
         let input = Input::godot_singleton();
         self.target_movement = 0.0;
         match self.side.as_ref() {
@@ -117,9 +111,9 @@ impl Paddle {
                 // TODO just to demonstrate that I can manipulate child nodes
                 // Remove later, maybe
                 if Input::is_action_pressed(&input, GodotString::from_str("ui_accept")) {
-                    self.sprite.set_visible(false);
+                    self.sprite.as_ref().unwrap().set_visible(false);
                 } else {
-                    self.sprite.set_visible(true);
+                    self.sprite.as_ref().unwrap().set_visible(true);
                 }
             }
             "right" => {
